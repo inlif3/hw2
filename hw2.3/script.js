18f5b7c0b32290e0ef0e7a04ba2f20aeaa9bc26d
Vadim Tlepov <tlepov@yandex.ru>
2023-01-31 23:25:40 +0300

    task 1

---
+++ hw2.3/script.js
@@ -0,0 +1,10 @@
+// Задание 1
+
+let password = 'пароль';
+let answerUser = prompt('Введите пароль');
+
+if (answerUser === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
