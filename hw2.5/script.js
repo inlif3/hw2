59e1446e062a3c5c4f6a44dc8f50f693bc4cffef
Vadim Tlepov <tlepov@yandex.ru>
2023-02-06 13:30:28 +0300

    task 2

--- hw2.5/script.js
+++ hw2.5/script.js
@@ -10,3 +10,12 @@ if (a < b) {
 
 console.log((8, 4));
 console.log((6, 6));
+
+// Задание 2
+
+function even_odd( number ) {
+return number % 2 == 0 ? "Число нечетное" : "Число четное"
+}
+
+console.log(even_odd(3));
+console.log(even_odd(4));
