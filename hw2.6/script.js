5ed5126b0977513baac02bbf5819c031df2d0f10
Vadim Tlepov <tlepov@yandex.ru>
2023-02-10 13:12:41 +0300

    task 3

--- hw2.6/script.js
+++ hw2.6/script.js
@@ -6,3 +6,22 @@ for (let i = 0; i < numb.length; i++) {
     if (numb[i] == 10) break;
     console.log(numb[i]);
 }
+
+// Задание 2
+
+let numbs = [1, 5, 4, 10, 0, 3];
+
+for (let elem of numbs) {
+    if (numbs[elem] === 4) {
+        console.log(elem);
+
+    }
+}
+
+// Задание 3
+
+let arr = [1, 3, 5, 10, 20];
+
+arr = arr.join(' ');
+
+console.log(arr);
